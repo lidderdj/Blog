7a59dad658c34b769f559985f8d596bfa54c14bd
lidderdj <lidderdj@gmail.com>
2019-09-25 21:09:24 +1000

    25092019 2109

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,14 +1,21 @@
 import React, {useContext} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext)
+  const blogPosts = useContext(BlogContext)
 
-    return <View>
-      <Text>Index</Text>
-      <Text>{value}</Text>
+    return (
+      <View>
+        <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPosts.title}
+        renderItem={({item}) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
       </View>
+    )
 }
 
 const styles = StyleSheet.create({})
